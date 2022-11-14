eb244fe7e2c721c9fe177cb4bd2c8a42883f3d0a
chelsea <crutgers021@student.wethinkcode.co.za>
2022-11-14 21:04:05 +0200

    update functionality and added docstrings

--- RPA/src/main/java/Game.java
+++ RPA/src/main/java/Game.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Game {
 
+    private static final Scanner game = new Scanner(System.in);
     private static String computer;
     private static String player;
 
@@ -10,42 +13,107 @@ public class Game {
 
     public static String compareChoices(String computer, String player) {
 
+        if (player.equals("exit") || player.equals("quit")){
+            System.out.println("Thanks for playing! Bye-Bye!");
+            System.exit(0);
+        }
+
         if (computer.equals("paper") && player.equals("rock")){
-            return "Computer wins!";
+            return String.format("Computer wins!\n" +computer+ " beats " + player);
         }else if (computer.equals("rock") && player.equals("scissors")){
-            return "Computer wins!";
+            return String.format("Computer wins!\n" + computer + " beats " + player);
         }else if (computer.equals("scissors") && player.equals("paper")){
-            return "Computer wins!";
+            return String.format("Computer wins!\n" + computer + " beats " + player);
         }else if (computer.equals(player)){
-            return "It's a tie!";
+            return String.format("It's a tie!\n" + computer + " / " + player);
         }
         else{
-            return "Player wins!";
+            return String.format("Player wins!\n"+ player + " beats " + computer);
         }
+    }
+
+    /**
+     * It takes input from user.
+     *
+     * @return The input from the user to set player name.
+     */
+    public static String enterName() {
+        System.out.println("\nPlease enter your name: ");
+        String input = game.nextLine();
+
+        while (input.isBlank()) {
+            System.out.println("\nYour Name should contain at least one letter.");
+            System.out.println("\nPlease enter your name: ");
+            input = game.nextLine();
+        }
+        return input.strip();
+    }
+
+    /**
+     * It takes input from user.
+     *
+     * @return The input from the user to set a new game.
+     */
+    public static String playAgain() {
+        System.out.println("\nwould you like to play again? yes/no ");
+        String again = game.nextLine();
+
+        while (again.isBlank()) {
+            System.out.println("\nPlay again? yes/no ");
+            again = game.nextLine();
+        }
+        return again.strip();
     }
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome Player!\n");
+        System.out.println("\n------->Welcome to Rock, Paper, Scissors game<-------");
+        
+        String playerName = enterName();
+
+        System.out.println("\nWelcome "+ playerName + "!");
 
         String playerMove;
-        if (Computer.moves.isEmpty()){
-            Computer.getMoves();
+
+        boolean continuePlaying = true;
+        int count = 0;
+
+        while (continuePlaying) {
+
+            while (count != 3){
+            
+                if (Computer.moves.isEmpty()){
+                    Computer.getMoves();
+                }
+
+                do{
+                    playerMove = Player.input();
+                }while(Player.validateInput(playerMove).equals("ERROR"));
+
+                count++;
+
+                Player.setPlayerMove(playerMove);
+
+                Computer.setComputerMove();
+                setMoves();
+
+                System.out.println("\n" + compareChoices(computer, player));
+                System.out.println(count);                    
+                
+            }
+
+            String anotherGame = playAgain();
+
+            if(anotherGame.equals("no")){
+                System.out.println("Thanks for playing! Bye-Bye!");
+                continuePlaying = false;
+                System.exit(0);
+            }else{
+                count = 0;
+                continue;
+            }
         }
 
-        do{
-            playerMove = Player.input();
-        }while(Player.validateInput(playerMove).equals("ERROR"));
-
-        Player.setPlayerMove(playerMove);
-
-        Computer.setComputerMove();
-        setMoves();
-
-        System.out.println("Player --> " + player +
-                "\nComputer -- > " + computer);
-        System.out.println("\n" + compareChoices(computer, player));
-
-
     }
+
 }
--- RPA/src/main/java/Player.java
+++ RPA/src/main/java/Player.java
@@ -7,12 +7,23 @@ public class Player {
     public static ArrayList<String> moves = new ArrayList<>();
     private static String playerMove;
 
+    /**
+     * Adds moves to list
+     */
     public static void getMoves() {
         moves.add("rock");
         moves.add("paper");
         moves.add("scissors");
+        moves.add("exit");
+        moves.add("quit");
     }
 
+    /**
+     * It takes input as a parameter.
+     *
+     * @return ERROR is input does not match list 
+     * of moves else returns OK.
+     */
     public static String validateInput(String input) {
         if (moves.isEmpty()){
             getMoves();
@@ -22,27 +33,32 @@ public class Player {
         }return "OK";
     }
 
+    /**
+     * Getter for playerMove.
+     */
     public static String getPlayerMove(){
         return playerMove;
     }
 
+    /**
+     * Setter for playerMove.
+     */
     protected static void setPlayerMove(String input) {
         playerMove = input;
     }
 
 
     /**
-     * It takes a string as an argument, prints it to the console, then waits for the user to enter a string, and returns
-     * that string
+     * It takes input from user.
      *
-     * @return The input from the user.
+     * @return The input from the user to set player move.
      */
     public static String input() {
-        System.out.println("Make your move: ");
+        System.out.println("\nMake your move: ");
         String input = sc.nextLine();
 
         while (input.isBlank()) {
-            System.out.println("Make your move: ");
+            System.out.println("\nInvalid move. Make your move: ");
             input = sc.nextLine();
         }
         return input.strip();
